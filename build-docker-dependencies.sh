81e96abcd7ad8c572a6e169d6a9a1171745f2396
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2018-08-28 18:06:29 +0300

    Staged build process replaced with a single Dockerfie

--- build-docker-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-for f in docker-base/*.dockerfile; do
-  # See https://stackoverflow.com/a/965072/3895471
-  filename=$(basename -- "$f")
-  filename_no_id=${filename:3}
-  tag="${filename_no_id%.*}"
-  echo "Building file $filename and tagging it with $tag"
-  docker build -t "tfpg-$tag" -f $@ $f .
-done
